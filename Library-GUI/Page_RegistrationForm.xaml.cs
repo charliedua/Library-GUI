e38d2111e1db80ffd204a307babae4ddc57c58c3
charliedua <duaanmol2@gmail.com>
2018-07-14 21:42:17 +1000

    Initial commit

---
+++ Library-GUI/Page_RegistrationForm.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Xml;
+using MySql.Data.MySqlClient;
+
+namespace Library_GUI
+{
+	/// <summary>
+	/// Interaction logic for Page_RegistrationForm.xaml
+	/// </summary>
+	public partial class Page_RegistrationForm : Page
+	{
+		public Page_RegistrationForm()
+		{
+			InitializeComponent();
+		}
+
+		private void Btn_Submit_Click(object sender, RoutedEventArgs e)
+		{
+			bool Submit = true;
+			string FullName = Text_FullName.Text;
+			DateTime DOB = new DateTime();
+
+			if (!Regex.IsMatch(FullName, @"[A-Za-z ]{3,25}"))
+			{
+				Label_NameHelper.Visibility = Visibility.Visible;
+				Label_NameHelper.Content = "Please Enter a Valid name";
+				Submit = false;
+			}
+			else
+			{
+				Label_NameHelper.Visibility = Visibility.Hidden;
+			}
+
+			if (!int.TryParse(Text_Age.Text, out int Age))
+			{
+				Label_AgeHelper.Visibility = Visibility.Visible;
+				Label_AgeHelper.Content = "Please enter an integer";
+				Submit = false;
+			}
+			else
+			{
+				Label_AgeHelper.Visibility = Visibility.Hidden;
+			}
+
+			if (Regex.IsMatch(Text_DOB.Text, @"[0-9]{2}-[0-9]{2}-[0-9]{4}"))
+			{
+				DOB = (DateTime)Text_DOB.SelectedDate;
+			}
+			else
+			{
+				Label_DOBHelper.Content = "Please Enter a valid date, or select from the calender";
+				Label_DOBHelper.Visibility = Visibility.Visible;
+				Submit = false;
+			}
+
+			if (Submit)
+			{
+				string connectionString = @"datasource=127.0.0.1;port=3306;database=Library_GUI;username=root;Password=;SslMode=none";
+				MySqlConnection conn = new MySqlConnection(connectionString);
+				try
+				{
+					conn.Open();
+				}
+				catch (Exception ex)
+				{
+					MessageBox.Show($"Connection cant be opend! \n error is {ex.Message.ToString()}",
+						@"Can't connect", MessageBoxButton.OK, MessageBoxImage.Error);
+					return;
+				}
+				string query = "CREATE TABLE IF NOT EXISTS members(" +
+					"membership_id int NOT NULL AUTO_INCREMENT PRIMARY KEY," +
+					"DOB date NOT NULL," +
+					"Name varchar(25) NOT NULL," +
+					"Age int NOT NULL" +
+					");";
+				MySqlCommand command = new MySqlCommand(query, conn);
+				try
+				{
+					command.ExecuteNonQuery();
+				}
+				catch (Exception ex)
+				{
+					MessageBox.Show($"Error executing query \n{ex.ToString()}", "error",
+						MessageBoxButton.OK, MessageBoxImage.Error);
+					conn.Close();
+					return;
+				}
+				query = $"INSERT INTO members (DOB, Name, Age) VALUES ({DOB.ToString("YYYY-MM-DD")}, '{Name}', {Age});";
+				command = new MySqlCommand(query, conn);
+				try
+				{
+					command.ExecuteNonQuery();
+				}
+				catch (Exception ex)
+				{
+					MessageBox.Show($"Error executing query \n{ex.ToString()}", "error",
+						MessageBoxButton.OK, MessageBoxImage.Error);
+					conn.Close();
+					return;
+				}
+				conn.Close();
+			}
+		}
+	}
+}
